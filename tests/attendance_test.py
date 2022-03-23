45b44f1fd485acc7397d7554137ba340686b6ceb
Young Joon Lee <yj.lee@chu.ac.kr>
2022-03-23 11:25:46 +0900

    update test

--- tests/attendance_test.py
+++ tests/attendance_test.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 from urllib.request import urlopen
 
@@ -6,8 +7,9 @@ url = 'https://raw.githubusercontent.com/chu-ise/assignment-template/main/assets
 def test_attendance():
     today = date.today().strftime("%Y%m%d")
     attendance_file = f"./attendance/{today}.txt"
-    with open(attendance_file) as f:
-        your_seed = f.readline().strip()
-    seed = urlopen(url).read().decode('utf-8').strip()
-    
-    assert your_seed == seed
+    if os.path.exists(attendance_file):
+        with open(attendance_file) as f:
+            your_seed = f.readline().strip()
+        seed = urlopen(url).read().decode('utf-8').strip()
+        
+        assert your_seed == seed
